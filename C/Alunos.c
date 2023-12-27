a652928f37b4db6f142a657e2eff7e2a5009a756
Pablosassis <assis.s.pablo@gmail.com>
2023-12-27 12:53:33 -0300

    Menu básico funcional

--- C/Alunos.c
+++ C/Alunos.c
@@ -4,48 +4,59 @@
 int escolha_inicio;
 char nome_busca[100];
 int i=0;
-//ESTRUTURA
+int numCadastro;
+//ESTRUTURAS
 typedef struct
 {
     char nome[200];
     char disciplina[100];
     float nota;
+    int CPF[11];
+    int RG[10];
 }Aluno;
 
-Aluno alunos[5];
+typedef struct
+{
+    char nome[200];
+    char rua[100];
+    int numero_rua;
+    int CEP[8];
+    Aluno dados_aluno[5];
+}Endereco_aluno;
+
+Endereco_aluno dados_endereco_aluno;
+
 //DEFINICOES DE FUNCOES
-void inicio();
+int inicio();
 void cadastro();
 void consulta();
 void editar();
 void deletar();
 
 //FUNÇÕES
-void inicio(){
+int inicio(){
     printf("\nDigite o que quer fazer?\n 1.Cadastro\n 2.Consulta\n 3.Editar\n 4.deletar\n O que pretende fazer: ");
-    scanf("%d%*c",escolha_inicio);
+    scanf("%d%*c",&escolha_inicio);
     fflush(stdin);
     switch (escolha_inicio)
     {
     case 1:
-        cadastro();
-        break;
-    
+        cadastro();    
     default:
-        break;
+        printf("erro");
     }
 }
 void cadastro(){
-    int numCadastro;
+    
     printf("\nQuantos voce quer cadastrar? ");
-    scanf("%i%*c",numCadastro);
+    scanf("%d%*c",&numCadastro);
     fflush(stdin);
     if (numCadastro>0)
     {
-        for (i = 0; i < 5; i++)
-        {
-        /* code */
-        }
+        // for (i = 0; i < 5; i++)
+        // {
+        // printf("%d",i);
+        // }
     }else if (numCadastro>5)
     {
         printf("\nLimite de 5 cadastros.");
@@ -53,9 +64,6 @@ void cadastro(){
     else{
         printf("\nValor inválido.");
     }
-    
-    
-     
 }
 void consulta(){
     
